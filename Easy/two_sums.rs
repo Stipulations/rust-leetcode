439dc056407923b9cac807db24d49f6b0fe38140
Stipulations <160319598+Stipulations@users.noreply.github.com>
2024-11-03 23:30:56 -0600

    Update two_sums.rs
    changed to meet lower latency

--- Easy/two_sums.rs
+++ Easy/two_sums.rs
@@ -1,11 +1,14 @@
+use std::collections::HashMap;
+
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        for i in 0..nums.len() {
-            for j in (i+1)..nums.len() {
-                if nums[i] + nums[j] == target {
-                    return vec![i as i32, j as i32];
-                }
+        let mut num_to_index = HashMap::new();
+        for (i, &num) in nums.iter().enumerate() {
+            let complement = target - num;
+            if let Some(&j) = num_to_index.get(&complement) {
+                return vec![j as i32, i as i32];
             }
+            num_to_index.insert(num, i);
         }
         vec![]
     }
